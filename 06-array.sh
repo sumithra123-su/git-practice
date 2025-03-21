38ede1687a5a8d417b38af5f9f8e699f772c8963
sumithra123-su <25gsumithra@gmail.com>
2025-03-21 17:33:08 +0530

    Types of fruits

---
+++ 06-array.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# index starts from 0 ,size is 3
+FRUITS=( "APPLE" "KIWI" "CHERRY" ) #array
+echo "first fruit is: ${FRUITS[0]}"
+echo "second fruit is: ${FRUITS[1]}"
+echo "third fruit is: ${FRUITS[2]}"
+
+echo "first fruit is: ${FRUITS[@]}"
