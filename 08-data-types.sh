788459ad95986129b79cc3650ec494b493086b0a
sumithra123-su <25gsumithra@gmail.com>
2025-03-23 14:18:54 +0530

    data types

---
+++ 08-data-types.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+NUMBER1=$1
+NUMBER2=$2
+
+SUM=$(($NUMBER1+$NUMBER2))
+PRODUCT=$(($NUMBER1*$NUMBER2))
+AVERAGE=$(($NUMBER1%$NUMBER2))
+
+echo "total number of $NUMBER1 and $NUMBER2 = $SUM"
+echo "total number of $NUMBER1 and $NUMBER2 = $PRODUCT"
+echo "total number of $NUMBER1 and $NUMBER2 = $AVERAGE"
