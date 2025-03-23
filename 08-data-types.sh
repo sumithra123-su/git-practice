a5bec97285907d9d1bf95358748e993ba0dfe12a
sumithra123-su <25gsumithra@gmail.com>
2025-03-23 15:19:18 +0530

    08-data types

--- 08-data-types.sh
+++ 08-data-types.sh
@@ -2,12 +2,9 @@
 
 NUMBER1=$1
 NUMBER2=$2
-COUNT1=$3
 
 SUM=$(($NUMBER1+$NUMBER2))
 PRODUCT=$(($NUMBER1*$NUMBER2))
-AVERAGE=$(echo "scale=2; (($NUMBER1+$NUMBER2) / $COUNT1)")
 
 echo "total number of sum $NUMBER1 and $NUMBER2 = $SUM"
 echo "total number of product $NUMBER1 and $NUMBER2 = $PRODUCT"
-echo "total number of average $NUMBER1 and $NUMBER2 = $AVERAGE"
