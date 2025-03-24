10d3b57eb22afb82cc25c63c2936c766237dc84f
sumithra123-su <25gsumithra@gmail.com>
2025-03-24 15:59:09 +0530

    if else condition

--- 11.if-else.sh
+++ 11.if-else.sh
@@ -25,3 +25,19 @@ else
     echo "git is already installerd, nothing to do"
 fi  
 
+dnf list install mysql -y
+
+if [ $? -ne 0 ]
+then
+echo "mysql nis not installed going to install it"
+dnf lnstall mysql -y
+  if [ $? -ne 0 ]
+     then
+       echo "mysql installation is not success, please check it"
+    else
+        echo "mysql installation is success"
+         exit 1
+   fi
+else
+    echo "mysql is already installerd, nothing to do"
+fi  
