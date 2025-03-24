613b55e0d776bfba698ae04b9fc14e4c332f07f1
sumithra123-su <25gsumithra@gmail.com>
2025-03-24 13:26:40 +0530

    conditions

---
+++ 10-conditions.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+NUMBER=$1
+
+if [ $NUMBER -gt 27 ] #gt,lt,eq,-ne,-ge,-le
+then
+echo "the given number: $NUMBER is greater than 27"
+else
+echo "the given number: $NUMBER is less than 27"
+fi
