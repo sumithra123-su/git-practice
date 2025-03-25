a11e727c790649cffc90d868b8f1ae46513a0ce3
sumithra123-su <25gsumithra@gmail.com>
2025-03-25 06:44:10 +0530

    functions

---
+++ 13-functions.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+USERID=$(id -u) 
+CHECK_ROOT()
+{
+  if [ $USERID -ne 0 ]
+    then
+     echo "please run this script with root priveleges"
+     exit 1
+   fi
+}
+VALIDATE()
+{
+    if [ $1 -ne 0 ]
+    then
+    echo "$2 is :faled"
+    else
+    echo "$2 is success"
+    fi
+}
+
+CHECK_ROOT
+
+dnf list install git -y
+
+if [ $? -ne 0 ]
+then
+echo "git nis not installed going to install it"
+dnf lnstall git -y
+  VALIDATE $? "installing git"
+else
+    echo "git is already installerd, nothing to do"
+fi  
+
+dnf list install mysql -y
+
+
+if [ $? -ne 0 ]
+then
+echo "mysql nis not installed going to install it"
+dnf lnstall mysql -y
+  VALIDATE $? "installing mysql"
+else
+    echo "mysql is already installerd, nothing to do"
+fi  
