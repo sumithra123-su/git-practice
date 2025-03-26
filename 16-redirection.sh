c0a4886dca326d491d30100800879e45875dabec
sumithra123-su <25gsumithra@gmail.com>
2025-03-26 22:57:15 +0530

    logs

--- 16-redirection.sh
+++ 16-redirection.sh
@@ -24,6 +24,7 @@ VALIDATE()
     if [ $1 -ne 0 ]
      then
         echo -e "$2  is..$R FAILED $N" &>>$LOG_FILE
+        exit 1
      else
          echo  -e "$2  is.. $G SUCCESS $N" &>>$LOG_FILE
     fi
