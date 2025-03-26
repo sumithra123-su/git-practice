59aabc78b8910ff202da45cd525b6f1f2ae20331
sumithra123-su <25gsumithra@gmail.com>
2025-03-26 22:15:37 +0530

    logs

--- 16-redirection.sh
+++ 16-redirection.sh
@@ -14,7 +14,7 @@ CHECK_ROOT()
 {
   if [ $USERID -ne 0 ]
     then
-     echo "please run this script with root priveleges"
+     echo -e "$R please run this script with root priveleges $N"  &>>$LOG_FILE
      exit 1
    fi
 }
@@ -22,9 +22,9 @@ VALIDATE()
 {
     if [ $1 -ne 0 ]
      then
-        echo -e "$2  is..$R FAILED $N"
+        echo -e "$2  is..$R FAILED $N" &>>$LOG_FILE
      else
-         echo  -e "$2  is.. $G SUCCESS $N"
+         echo  -e "$2  is.. $G SUCCESS $N" &>>$LOG_FILE
     fi
 }
 
