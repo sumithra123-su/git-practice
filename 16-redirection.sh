a8a2f7cd330cdd3c3bbafca70f5b70729cd28535
sumithra123-su <25gsumithra@gmail.com>
2025-03-27 22:02:24 +0530

    redirection

--- 16-redirection.sh
+++ 16-redirection.sh
@@ -11,11 +11,12 @@ R="\e[31m"
 G="\e[32m"
 N="\e[0m" 
 Y="\e[33"
+
 CHECK_ROOT()
 {
   if [ $USERID -ne 0 ]
     then
-     echo -e "$R please run this script with root priveleges $N"  &>>$LOG_FILE
+     echo -e "$R please run this script with root priveleges $N"  | tee -a $LOG_FILE
      exit 1
    fi
 }
@@ -23,10 +24,10 @@ VALIDATE()
 {
     if [ $1 -ne 0 ]
      then
-        echo -e "$2  is..$R FAILED $N" &>>$LOG_FILE
+        echo -e "$2  is..$R FAILED $N" | tee -a $LOG_FILE
         exit 1
      else
-         echo  -e "$2  is.. $G SUCCESS $N" &>>$LOG_FILE
+         echo  -e "$2  is.. $G SUCCESS $N" | tee -a $LOG_FILE
     fi
 }
 USAGE()
@@ -34,7 +35,7 @@ USAGE()
     echo -e "$R USAGE:: $N sudo sh 16.redirection.sh package1 package2 ...."
     exit 1
 }
-echo "script started execting at :$(date)" &>>$LOG_FILE
+echo "script started execting at :$(date)" | tee -a $LOG_FILE
 
 CHECK_ROOT
 
@@ -45,14 +46,14 @@ fi
  
 for package in $@ # $@ refers all arguments passed to it
  do
-  dnf list install $package   &>>$LOG_FILE
+  dnf list installed $package   &>>$LOG_FILE
 
    if [ $? -ne 0 ]
       then
-        echo "$package nis not installed going to install it" &>>$LOG_FILE
+        echo "$package nis not installed, going to install it.." | tee -a $LOG_FILE
         dnf lnstall $package -y &>>$LOG_FILE
         VALIDATE $? "installing $package"
       else
-        echo -e "$package is already installerd,$Y nothing to do it $N" &>>$LOG_FILE   
+        echo -e "$package is already $Y installerd.. nothing to do it $N" | tee -a $LOG_FILE   
      fi
 done   
